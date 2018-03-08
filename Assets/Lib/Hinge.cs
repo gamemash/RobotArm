c81902e0e6fb5904dfc29f87d4c1415e59db4b54
ronald hecker <ronald.m.hecker@gmail.com>
2018-03-08 20:26:09 +0100

    Initial Import
    Added robot arm model and a hinge object to define the robot movements

---
+++ Assets/Lib/Hinge.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Lib
+{
+    public class Hinge
+    {
+        private GameObject gameObject;
+        private RangeInt range;
+        private Vector3 direction;
+        private float angularVelocity;
+        
+        public Hinge(GameObject gameObject, Vector3 direction,  RangeInt range, float angularVelocity)
+        {
+            this.gameObject = gameObject;
+            this.direction = direction;
+            this.range = range;
+            this.angularVelocity = angularVelocity;
+        }
+
+        public void moveTo(float degree)
+        {
+            var currentAngle = getComponentOfVector3(gameObject.transform.localEulerAngles, direction);
+            var distance = (clamp((degree + 360) % 360, range.start, range.length) - currentAngle) % 360;
+            var dAngle = clamp(distance, -angularVelocity * Time.deltaTime, angularVelocity * Time.deltaTime);
+            if (Math.Abs(dAngle) > 0.01)
+            {
+                gameObject.transform.Rotate(direction, dAngle);
+            }
+        }
+
+        private float clamp(float val, float min, float max)
+        {
+            if (val < min)
+                return min;
+            else if (val > max)
+                return max;
+            else return val;
+        }
+
+        private float getComponentOfVector3(Vector3 vector, Vector3 d)
+        {
+            return new Vector3(
+                vector.x * d.x,
+                vector.y * d.y,
+                vector.z * d.z).magnitude;
+        }
+    }
+}
---
+++ Assets/RobotArm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lib;
+using UnityEngine;
+
+public class RobotArm : MonoBehaviour {
+	public GameObject baseConnector;
+	private Hinge baseHinge;
+	public GameObject upperArm;
+	private Hinge upperArmHinge;
+	public GameObject lowerArm;
+	private Hinge lowerArmHinge;
+	public GameObject wristRotate;
+	public GameObject wristHinge;
+	public GameObject grabber;
+
+	// Use this for initialization
+	void Start ()
+	{
+		baseHinge = new Hinge(baseConnector, Vector3.forward, new RangeInt(0, 360), 40.0f);
+		upperArmHinge = new Hinge(upperArm, Vector3.up, new RangeInt(-110, 40), 40.0f);
+		lowerArmHinge = new Hinge(lowerArm, Vector3.up, new RangeInt(0, 360), 40.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		baseHinge.moveTo(60);
+		upperArmHinge.moveTo(40);
+		lowerArmHinge.moveTo(180);
+		// upperArm.transform.Rotate(Vector3.down, 0.02f);
+	}
+}
