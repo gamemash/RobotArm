5d334a84e739bbebca1f92dba33b246be9a9af59
ronald hecker <ronald.m.hecker@gmail.com>
2018-03-09 12:46:51 +0100

    Hinge now works properly with range limited hinges.
    Ranges that "loop around", 270deg to 90deg for instance.
    It will now also try to find the closest it can get.

--- Assets/Lib/Hinge.cs
+++ Assets/Lib/Hinge.cs
@@ -10,24 +10,58 @@ namespace Lib
         private RangeInt range;
         private Vector3 direction;
         private float angularVelocity;
-        
-        public Hinge(GameObject gameObject, Vector3 direction,  RangeInt range, float angularVelocity)
+        private bool rangeLimited;
+
+        public Hinge(GameObject gameObject, Vector3 direction, RangeInt range, float angularVelocity)
         {
             this.gameObject = gameObject;
             this.direction = direction;
             this.range = range;
             this.angularVelocity = angularVelocity;
+            rangeLimited = (range.length - range.start) < 360;
         }
 
         public void moveTo(float degree)
         {
-            var currentAngle = getComponentOfVector3(gameObject.transform.localEulerAngles, direction);
-            var distance = (clamp((degree + 360) % 360, range.start, range.length) - currentAngle) % 360;
+            var currentAngle = getDirectionComponentOfVector3(gameObject.transform.localEulerAngles);
+            float deg;
+            float distance;
+            if (rangeLimited) {
+                deg = degree;
+                if (!withinRange(degree, range.start, range.length)) {
+                    Debug.Log("Degree is out of range!");
+                    // Find closest to the point you can get.
+                    if (Math.Abs(degree - range.start) > Math.Abs(degree - range.length)) {
+                        deg = range.length;
+                    } else {
+                        deg = range.start;
+                    }
+                }
+
+                distance = (deg - currentAngle);
+                // Loop around if the range is looping past 360
+                if (range.start > range.length && distance > 180){
+                    distance -= 360;
+                }
+            } else {
+                deg = (degree + 360) % 360;
+                distance = (deg - currentAngle);
+                if (distance > 180)
+                    distance -= 360;
+            }
+            
             var dAngle = clamp(distance, -angularVelocity * Time.deltaTime, angularVelocity * Time.deltaTime);
-            if (Math.Abs(dAngle) > 0.01)
-            {
+            if (Math.Abs(dAngle) > 0.01) {
                 gameObject.transform.Rotate(direction, dAngle);
             }
+        }
+
+        private bool withinRange(float val, float min, float max)
+        {
+            if (min > max)
+                return val > min && val < max + 360;
+            else
+                return val > min && val < max;
         }
 
         private float clamp(float val, float min, float max)
@@ -39,12 +73,12 @@ namespace Lib
             else return val;
         }
 
-        private float getComponentOfVector3(Vector3 vector, Vector3 d)
+        private float getDirectionComponentOfVector3(Vector3 vector)
         {
             return new Vector3(
-                vector.x * d.x,
-                vector.y * d.y,
-                vector.z * d.z).magnitude;
+                vector.x * direction.x,
+                vector.y * direction.y,
+                vector.z * direction.z).magnitude;
         }
     }
 }
--- Assets/RobotArm.cs
+++ Assets/RobotArm.cs
@@ -1,33 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Lib;
 using UnityEngine;
 
 public class RobotArm : MonoBehaviour {
 	public GameObject baseConnector;
-	private Hinge baseHinge;
 	public GameObject upperArm;
-	private Hinge upperArmHinge;
 	public GameObject lowerArm;
-	private Hinge lowerArmHinge;
 	public GameObject wristRotate;
 	public GameObject wristHinge;
 	public GameObject grabber;
+	
+	private Hinge baseHinge;
+	private Hinge upperArmHinge;
+	private Hinge lowerArmHinge;
+	private Hinge wristRotateHinge;
+	private Hinge wristHingeHinge;
+	private Hinge grabberHinge;
+	
+
+
+	public enum Hinges
+	{
+		Base, UpperArm, LowerArm, WristRotate, WristHinge, Grabber
+	}
 
 	// Use this for initialization
 	void Start ()
 	{
-		baseHinge = new Hinge(baseConnector, Vector3.forward, new RangeInt(0, 360), 40.0f);
-		upperArmHinge = new Hinge(upperArm, Vector3.up, new RangeInt(-110, 40), 40.0f);
-		lowerArmHinge = new Hinge(lowerArm, Vector3.up, new RangeInt(0, 360), 40.0f);
+		baseHinge 			= new Hinge(baseConnector, Vector3.forward, new RangeInt(0, 360), 60.0f);
+		upperArmHinge 		= new Hinge(upperArm, Vector3.up, new RangeInt(270, 40), 40.0f);
+		lowerArmHinge 		= new Hinge(lowerArm, Vector3.up, new RangeInt(0, 360), 40.0f);
+		wristRotateHinge 	= new Hinge(wristRotate, Vector3.forward, new RangeInt(0, 360), 40.0f);
+		wristHingeHinge 	= new Hinge(wristHinge, Vector3.up, new RangeInt(0, 360), 40.0f);
+		// grabberHinge = new Hinge(grabber, Vector3.);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		baseHinge.moveTo(60);
-		upperArmHinge.moveTo(40);
-		lowerArmHinge.moveTo(180);
-		// upperArm.transform.Rotate(Vector3.down, 0.02f);
+		MoveHinge(Hinges.Base, 180);
+		MoveHinge(Hinges.UpperArm, 180);
+		MoveHinge(Hinges.LowerArm, 50);
 	}
+
+	public void MoveHinge(Hinges hinge, float degree)
+	{
+		switch (hinge)
+		{
+			case Hinges.Base:
+				baseHinge.moveTo(degree);
+				break;
+			case Hinges.UpperArm:
+				upperArmHinge.moveTo(degree);
+				break;
+			case Hinges.LowerArm:
+				lowerArmHinge.moveTo(degree);
+				break;
+			case Hinges.WristRotate:
+				wristRotateHinge.moveTo(degree);
+				break;
+			case Hinges.WristHinge:
+				wristHingeHinge.moveTo(degree);
+				break;
+		}
+	}
+
+
 }
